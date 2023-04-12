723682243d89d2de42a2cf70cff7605a029950e1
Ryo Nakatani <ryo.nakatani@oist.jp>
2023-04-12 18:18:05 +0900

    Finished project for finding better parameter experience

--- results/utils/optSynWeight.py
+++ results/utils/optSynWeight.py
@@ -79,8 +79,7 @@ def saveTau(t,A,B,i,dName='./results/optimize/'):
     sFile = open(fName,'r')
     return sFile.readlines()
 
-
-def resLikelihood(expData='./Data/VClamp40.1Stim.dat'):
+def relLikelihood(expData='./Data/VClamp40.1Stim.dat',scale=2):
     iCurve = pd.read_csv('iFile.dat',header=None,names=['current'])
     tSample = pd.read_csv('tFile.dat',header=None,names=['t'])
 
@@ -92,7 +91,28 @@ def resLikelihood(expData='./Data/VClamp40.1Stim.dat'):
         t,v = line.strip().split()
         tList.append(float(t))
         vList.append(float(v))
-    itResult = [(t,i) for i,t in zip(iCurve['current'],tSample['t']) if t in tList]
+    itResult = [(t,i/max(iCurve['current'])) for i,t in zip(iCurve['current'],tSample['t']) if t in tList]
+    vList = [v/max(vList) for v in vList]
+    rss = 0
+    for i,current in enumerate(itResult):
+        t,c = current
+        rss += (c - vList[i]) ** 2
+    return rss * abs(max(iCurve['current'])/scale-max(vList)) / max(vList) * 1e4
+
+def resLikelihood(expData='./Data/VClamp40.1Stim.dat'):
+    scale = 1 # Surface area of patch membrane 2um to PAP area
+    iCurve = pd.read_csv('iFile.dat',header=None,names=['current'])
+    tSample = pd.read_csv('tFile.dat',header=None,names=['t'])
+
+    fName = open(expData,'r')
+    lines = fName.readlines()
+    tList = []
+    vList = []
+    for line in lines[2:]:
+        t,v = line.strip().split()
+        tList.append(float(t))
+        vList.append(float(v))
+    itResult = [(t,i/scale) for i,t in zip(iCurve['current'],tSample['t']) if t in tList]
     vList = [v for v in vList]
     rss = 0
     for i,current in enumerate(itResult):
@@ -121,34 +141,37 @@ def objective(trial):
 
     """
     # Save Synaptic weight
-    SynWeight = trial.suggest_float("SynWeight", 0, 1)
+    SynWeight = trial.suggest_float("SynWeight", 0, 1e-5)
     saveSynWeight(SynWeight)
 
-    # Suggest DELTA
+    # # Suggest DELTA
     DELTA = trial.suggest_float("DELTA", 0, 1)
     saveDelta(DELTA)
 
-    # suggest Tau1
-    tau1 = trial.suggest_float("tau1", 0, 100)
-    wTau2 = trial.suggest_float("wTau2", 0, 1)
-    saveTau(tau1,wTau2,None,1)
+    # # suggest Tau1
+    # tau1 = trial.suggest_float("tau1", 0, 10)
+    # wTau2 = trial.suggest_float("wTau2", 0, 0.1)
+    # saveTau(tau1,wTau2,None,1)
     
     # suggest Tau2
     tau2 = trial.suggest_float("tau2", 0, 100)
-    A2 = trial.suggest_float("A2", 0, 100)
-    B2 = trial.suggest_float("B2", 0, 1)
+    A2 = trial.suggest_float("A2", 0, 50)
+    B2 = trial.suggest_float("B2", 0, 0.1)
     saveTau(tau2,A2,B2,2)
     
     # suggest Tau3
     tau3 = trial.suggest_float("tau3", 0, 100)
-    A3 = trial.suggest_float("A3", 0, 100)
-    B3 = trial.suggest_float("B3", 0, 1)
+    A3 = trial.suggest_float("A3", 0, 500)
+    B3 = trial.suggest_float("B3", 0, 0.1)
     saveTau(tau3,A3,B3,3)
+
+    scale = trial.suggest_int("scale",1,50)
     
     h.load_file("Exp08-NaSpike-ExpSyn.hoc")
     
+    
 
-    score = resLikelihood()
+    score = relLikelihood(scale=scale) #Score based on shape also
     return score
 
 
@@ -181,11 +204,11 @@ def setBest(study):
 
     saveDelta(parmDict['DELTA'])
 
-    saveTau(parmDict['tau1'],
-        parmDict['wTau2'],
-        None,
-        1
-    )
+    # saveTau(parmDict['tau1'],
+    #     parmDict['wTau2'],
+    #     None,
+    #     1
+    # )
 
     saveTau(parmDict['tau2'],
             parmDict['A2'],
--- results/utils/visualize.py
+++ results/utils/visualize.py
@@ -1,3 +1,4 @@
+
 """
 Author: Joel Nakatani
 Overview:
@@ -18,18 +19,19 @@ def loadFile(fName):
     else:
         return None
 
-def func(dir,zoom=False,ext=True):
+def func(dir,zoom=False,ext=False):
     i = loadFile(os.path.join(dir,'iFile.dat'))
     v = loadFile(os.path.join(dir,'vFile.dat'))
     mem = loadFile(os.path.join(dir,'iFileMem.dat'))
     t = loadFile(os.path.join(dir,'tFile.dat'))
     plt.plot(t,i)
-    plt.ylabel('nA')
-    # plt.ylim(-10,0)
+    plt.ylabel('pA')
+    if max(i.iloc[:, 0]) > 1:
+        plt.ylim(0,25)
     plt.xlabel('ms')
     if zoom:
-        plt.xlim(9.99,10.02)
-    elif ext:
+        plt.xlim(9.99,10.025)
+    elif ext and max(t) >= 10000:
         plt.xlim(11,10000)
     plt.savefig(os.path.join(dir,'results.pdf'))
     plt.cla()
@@ -38,23 +40,24 @@ def func(dir,zoom=False,ext=True):
         plt.plot(t,v)
         plt.ylabel('mV')
         plt.xlabel('ms')
-        # plt.ylim(-90,0)
-        if zoom: 
-            plt.xlim(9.99,10.02)
-        elif ext:
-            plt.xlim(11,10000)
+        print(max(v.iloc[:,0]))
+        plt.ylim(-90,0)
+        # if zoom or max(v.iloc[:, 0]) > -20: 
+        #     plt.xlim(9.99,10.1)
+        # elif ext and max(t) >= 10000:
+        #     plt.xlim(11,10000)
             
         plt.savefig(os.path.join(dir,'resultsV.pdf'))
         plt.cla()
         plt.clf()
     if mem is not None:
         plt.plot(t,mem)
-        plt.ylabel('nA')
+        plt.ylabel('pA')
         plt.xlabel('ms')
         # plt.ylim(-90,0)
         if zoom:
-            plt.xlim(9.99,10.02)
-        elif ext:
+            plt.xlim(9.99,10.025)
+        elif ext and max(t) >= 10000:
             plt.xlim(11,10000)
 
         plt.savefig(os.path.join(dir,'resultsIMem.pdf'))
