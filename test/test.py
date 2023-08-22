11520320133df2ddd1c4516db0db3425ae269a29
Ryo Nakatani <ryo.nakatani@oist.jp>
2023-08-22 16:22:53 +0900

    Added testing framework for NEURON

--- test/test.py
+++ test/test.py
@@ -11,10 +11,11 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 import numpy as np
 import pickle
+import os
 
 class cell():
     soma = object()
-    tstop = 100
+    tstop = 200
     dt = 0.01
     celsius = 37
     v_init = -85
@@ -111,17 +112,25 @@ class cell():
     def overexpressionKir(self, multiple):
         # captures reducing depolarization
         for seg in self.soma:
-            seg.kir4.Pkir = multiple * seg.kir4.Pkir
-        
+            seg.kir4.Pkir = seg.kir4.Pkir/multiple # assumption that Pk change is smaller than change of U(max)
+            if seg.kir4.Pkir > 1.0:
+                seg.kir4.Pkir = 1.0
+
+    def overexpressionTwik(self, multiple):
+        # captures reducing depolarization
+        for seg in self.soma:
+            seg.twik.PBkp = seg.twik.PBkp * multiple
+            if seg.twik.PBkp > 1.0:
+                seg.twik.PBkp = 1.0
+
+
 def testKoConc():
     astrocyte = cell()
-    astrocyte.channels(astrocyte.soma,twik=False)
+    astrocyte.channels(astrocyte.soma)
     astrocyte.record()
-
     for Kconc in [2.5,5,8.5]:
         astrocyte.setK(Kconc)
         print(astrocyte.soma.psection())
-        # astrocyte.overexpressionKir(30)
         astrocyte.run()
         # plt.plot(astrocyte.time,astrocyte.vSoma)
         print(astrocyte.soma.psection())
@@ -129,24 +138,108 @@ def testKoConc():
         print(max(np.array(astrocyte.vSoma) + 85))
         plt.xlabel('time (ms)')
         plt.ylabel('voltage (mV)')
-        plt.savefig(f'voltagteKo{Kconc}.pdf')
+    fname = f'Depolarization_Ko{Kconc}_ALL.pdf'
+    dirName = os.path.join(".") #,"results","SOMA","SOMATIC_KIROE") # To Do make title selection more smarter
+    plt.savefig(os.path.join(dirName,fname))
+
+                
+def testOVERExpression(**kwargs):
+    astrocytes = []
+    for multiple in np.arange(1,500,50):
+        astrocytes.append(cell())
+        astrocytes[-1].channels(astrocytes[-1].soma,
+                                **kwargs
+                                )
+                           
+        astrocytes[-1].record()
+
+        for Kconc in [2.5,5,8.5]:
+            astrocytes[-1].setK(Kconc)
+            print(astrocytes[-1].soma.psection())
+            if 'kir4' in kwargs.keys() and kwargs['kir4']:
+                astrocytes[-1].overexpressionKir(multiple)
+            astrocytes[-1].run()
+            # plt.plot(astrocytes[-1].time,astrocytes[-1].vSoma)
+            print(astrocytes[-1].soma.psection())
+            plt.plot(astrocytes[-1].time,astrocytes[-1].vSoma)
+            print(max(np.array(astrocytes[-1].vSoma) + 85))
+            plt.xlabel('time (ms)')
+            plt.ylabel('voltage (mV)')
+            fname = f'Depolarization_Ko{Kconc}_KIROE_{multiple}.pdf'
+            dirName = os.path.join(".","results","SOMA","SOMATIC_KIROE") # To Do make title selection more smarter
+            plt.savefig(os.path.join(dirName,fname))
+
+    SSVoltage = [ astrocyte.vSoma[-1] for astrocyte in astrocytes ] # Shoule be all 8.5 mM
+    plt.cla()
+    plt.clf()
+    plt.plot(np.arange(1,500,50),SSVoltage)
+    plt.xlabel('Relative Overexpression')
+    plt.ylabel('depolarization at 8.5 mM K+')
+    fname = f'Depolarization_KIROE_ALL.pdf'
+    plt.savefig(os.path.join(dirName,fname))
+
+def testKDExpression(**kwargs):
+    astrocytes = []
+    for multiple in np.arange(1,500,50):
+        astrocytes.append(cell())
+        astrocytes[-1].channels(astrocytes[-1].soma,
+                                **kwargs
+                                )
+                           
+        astrocytes[-1].record()
+
+        for Kconc in [2.5,5,8.5]:
+            astrocytes[-1].setK(Kconc)
+            print(astrocytes[-1].soma.psection())
+            if 'kir4' in kwargs.keys() and kwargs['kir4']:
+                astrocytes[-1].overexpressionKir(1/multiple)
+            if 'twik' in kwargs.keys() and kwargs['twik']:
+                astrocytes[-1].overexpressionTwik(1/multiple)
+            astrocytes[-1].run()
+            # plt.plot(astrocytes[-1].time,astrocytes[-1].vSoma)
+            print(astrocytes[-1].soma.psection())
+            plt.plot(astrocytes[-1].time,astrocytes[-1].vSoma)
+            print(max(np.array(astrocytes[-1].vSoma) + 85))
+            plt.xlabel('time (ms)')
+            plt.ylabel('voltage (mV)')
+            fname = f'Depolarization_Ko{Kconc}_KIRKD_{multiple}.pdf'
+            dirName = os.path.join(".","results","SOMA","SOMATIC_TWIKKO") # To Do make title selection more smarter
+            plt.savefig(os.path.join(dirName,fname))
+
+    SSVoltage = [ astrocyte.vSoma[-1] for astrocyte in astrocytes ] # Shoule be all 8.5 mM
+    plt.cla()
+    plt.clf()
+    plt.plot(np.arange(1,500,50),SSVoltage)
+    plt.xlabel('Relative KD')
+    plt.ylabel('depolarization at 8.5 mM K+')
+    fname = f'Depolarization_KIROE_ALL.pdf'
+    plt.savefig(os.path.join(dirName,fname))
+    
+        
 
 def eq(x,a,b):
     return a * x + b
 
 def measureCond(fName,twikExpr,kirExpr,gmax=True):
-    voltList = np.arange(-150,50,5)
+    vStep = 5
+    voltList = np.arange(-80,100,vStep)
     IV = {}
     astrocyte = cell()
     astrocyte.channels(astrocyte.soma,
-                       twik=twikExpr,
-                       kir4=kirExpr,
-                       K_acc=False,
-                       kleak=False,
-                       kdifl=False
+                       twik=True,
+                       kir4=True,
+                       K_acc=True,
+                       kleak=True,
+                       kdifl=True
                        )
     astrocyte.record()
     astrocyte.setK(50,initialKi=130)
+    if not twikExpr:
+        astrocyte.overexpressionTwik(1/100)
+    if not kirExpr:
+        astrocyte.overexpressionKir(1/100)
+        
+
     
     for volt in voltList:
         print(volt)
@@ -156,37 +249,61 @@ def measureCond(fName,twikExpr,kirExpr,gmax=True):
         pickle.dump(IV, handle, protocol=pickle.HIGHEST_PROTOCOL)
     I = list(IV.values())
     V = list(IV.keys())
-    plt.plot(V,I,label='model')
+    # plt.plot(V,I,label=f'{fName}')
     if gmax:
         # print(np.array(list(zip(I,V))))
-        gmaxValue = max(np.gradient(np.array(I)))/5
+        gmaxValue = max(np.gradient(np.array(I)))/vStep
         b = I[0] - gmaxValue * V[0]
         popt = [gmaxValue,b]
+        print(gmaxValue)
+        # gVals  = [ i / (v + 85) for i,v in zip(I,V)] 
+        # gmaxValue = abs(max(gVals,key=abs))
         
     else:
         popt, pcov = curve_fit(eq,V,I)
     x = np.linspace(-150, 50, 50)
-    print(popt)
-    # plt.plot(x,eq(x,*popt),label=f'{popt[0]:.2f}x+{popt[1]:.2f}')
+    # print(popt)
+    # plt.plot(x,eq(x,*popt),label=f'gmax slope:{popt[0]:.2f}x+{popt[1]:.2f}')
+    # plt.legend()
+    # plt.xlabel('voltage (mv)')
+    # plt.ylabel('current (nA)')
+    # plt.savefig(f"{fName}.pdf")
+    # plt.cla()
+    # plt.clf()
+    relG = np.gradient(np.array(I))/max(np.gradient(np.array(I)))
+    plt.scatter(V,relG,label=f'{fName}')
+    # plt.plot(V,gVals/gmaxValue,label=f'{fName}')
     plt.legend()
-    plt.savefig(f"{fName}.pdf")
+    plt.xlabel('voltage (mv)')
+    plt.ylabel('relative conductance (g/gmax)')
+    plt.savefig(f"{fName}_relG.pdf")
+    # plt.cla()
+    # plt.clf()
 
     
 
 if __name__ == "__main__":
-    astrocyte = cell()
-    astrocyte.channels(astrocyte.soma,
-                       twik=True,
-                       kir4=False,
-                       K_acc=False,
-                       kleak=False,
-                       kdifl=False
-                       )
-    astrocyte.record()
-    astrocyte.setK(50,initialKi=130)
-    astrocyte.run(clamp=-100,mode=1)
-    plt.plot(astrocyte.time,astrocyte.iKSoma)
-    plt.show()
-    # measureCond('Control',True,True)
-    # measureCond('TWIKKO',False,True)
+    # testKDExpression(
+    #     twik=True,
+    #     kir4=False,
+    #     K_acc=False,
+    #     kleak=False,
+    #     kdifl=False
+    # )
+    # astrocyte = cell()
+    # astrocyte.channels(astrocyte.soma,
+    #                    twik=True,
+    #                    kir4=True,
+    #                    K_acc=False,
+    #                    kleak=False,
+    #                    kdifl=False
+    #                    )
+    # astrocyte.record()
+    # astrocyte.setK(50,initialKi=130)
+    # astrocyte.run(clamp=30,mode=1)
+    # plt.plot(astrocyte.time,astrocyte.iKSoma)
+    # plt.show()
+    measureCond('Control',True,True)
+    measureCond('TWIKKO',False,True)
     # measureCond('KIRKO',True,False)
+    # testKoConc()
